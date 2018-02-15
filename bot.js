499005daca1a89074d09631b9f7ac27a95977b16
ItzRean <36505303+ItzRean@users.noreply.github.com>
2018-02-15 14:19:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'Rean') {
+    	message.reply('Is Our God Developer we need him in our life');
   	}
 });
 
