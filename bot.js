f7fc88fa376e8306d0a7cc387ada4e50f5568cb9
ItzRean <36505303+ItzRean@users.noreply.github.com>
2018-02-16 10:38:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,9 +19,6 @@ client.on("message", (message) => {
     if (message.content.startsWith("!credit")) {
     message.channel.send(" ```Credits to: Tami, Omicron,TheLostName For Testing``` ");
   }
-      if (message.content.startsWith("!buy")) {
-    message.channel.send(" ```Please dm rean#4356 ``` ");
-  }
         if (message.content.startsWith("!booty")) {
     message.channel.send(" https://gyazo.com/796975f0cdaeb7f184b7965709825a79 ");
   }
@@ -30,6 +27,12 @@ client.on("message", (message) => {
   }
         if (message.content.startsWith("!hkq")) {
     message.channel.send(" ```Darling , you are a goddess and once you know what that truly means, i pray for anyone who tries to hurt you ```");
+  }
+        if (message.content.startsWith("!rean")) {
+    message.channel.send(" ```The Developer of the bod and our holy king ```");
+  }
+          if (message.content.startsWith("!omicron")) {
+    message.channel.send(" ```A Huge Fucking Giant!```");
   }
                               
   
