9051eaf49afa5933cbdf6c2819af1fe5ac7d787d
ItzRean <36505303+ItzRean@users.noreply.github.com>
2018-02-16 10:57:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,29 +13,37 @@ client.on("message", (message) => {
   if (message.content.startsWith("!version")) {
     message.reply(" ```ReanBot Version: 0.0.3``` ");
   }
-    if (message.content.startsWith("!developer")) {
+  if (message.content.startsWith("!developer")) {
     message.reply(" ```The developer is Rean#4356``` ");
   }
-    if (message.content.startsWith("!credit")) {
+  if (message.content.startsWith("!credit")) {
     message.channel.send(" ```Credits to: Tami, Omicron,TheLostName For Testing``` ");
   }
-        if (message.content.startsWith("!booty")) {
+  if (message.content.startsWith("!booty")) {
     message.channel.send(" https://gyazo.com/796975f0cdaeb7f184b7965709825a79 ");
   }
-        if (message.content.startsWith("!ineedfriends")) {
+  if (message.content.startsWith("!ineedfriends")) {
     message.channel.send(" ```Thats gone be $50 for 1 fake friend!```");
   }
-        if (message.content.startsWith("!hkq")) {
+  if (message.content.startsWith("!hkq")) {
     message.channel.send(" ```Darling , you are a goddess and once you know what that truly means, i pray for anyone who tries to hurt you ```");
   }
-        if (message.content.startsWith("!rean")) {
+  if (message.content.startsWith("!rean")) {
     message.channel.send(" ```The Developer of the bod and our holy king ```");
   }
-          if (message.content.startsWith("!omicron")) {
+  if (message.content.startsWith("!omicron")) {
     message.channel.send(" ```A Huge Fucking Giant!```");
-  }
-                              
+  }                           
   
+});
+   
+   bot.on("guildMemberAdd", member => {
+       console.log("user " + member.user.username + " Has joined the server")
+     
+       var role = member.guild.roles("name", "User");
+     
+       member.addRole(role)
+
 });
 
 // THIS  MUST  BE  THIS  WAY
