0152e335d66ced59d7116a52f2c635e0217299e8
ItzRean <36505303+ItzRean@users.noreply.github.com>
2018-02-16 13:51:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const Discord = require('discord.js');
-
-const embed = new Discord.RichEmbed();
-embed.setTitle('This is an example title');
-embed.setColor('#4caf50');                                        // A Hex color
-embed.setDescription('A description that supports `markdown`');
-embed.setURL('https://codebottle.io/');                           // A URL to include
-embed.setAuthor('OverCoder');                                     // Author of the object 
-
-// Send the embed to `channel`
-channel.sendEmbed(general);
-});
 
 client.on("message", (message) => {
   if (message.content.startsWith("!friends")) {
@@ -58,7 +46,20 @@ client.on("message", (message) => {
   if (message.content === '!avatar') {
     message.reply(message.author.avatarURL);
   }
+});
   
+const Discord = require('discord.js');
+
+const embed = new Discord.RichEmbed();
+embed.setTitle('User');
+embed.setColor('#4caf50');                                        // A Hex color
+embed.setDescription('A description that supports `markdown`');
+embed.setURL('https://codebottle.io/');                           // A URL to include
+embed.setAuthor('OverCoder');                                     // Author of the object 
+
+// Send the embed to `channel`
+channel.sendEmbed(general);
+
 });
 
 client.on('guildMemberAdd', member => {
