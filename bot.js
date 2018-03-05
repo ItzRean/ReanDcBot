39e4036b3ce727dca53e830a94531b5409870469
ItzRean <36505303+ItzRean@users.noreply.github.com>
2018-03-05 16:38:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,53 +11,29 @@ client.on("message", (message) => {
     message.reply("Hai!");
   }
   if (message.content.startsWith("!version")) {
-    message.reply(" ```ReanBot Version: 0.0.7``` ");
+    message.reply(" ```Insife Version: 0.0.1``` ");
   }
   if (message.content.startsWith("!developer")) {
-    message.reply(" ```The developer is Rean#4356``` ");
-  }
-  if (message.content.startsWith("!credit")) {
-    message.channel.send(" ```Credits to: Tami, Omicron,TheLostName For Testing``` ");
-  }
-  if (message.content.startsWith("!booty")) {
-    message.channel.send(" https://gyazo.com/796975f0cdaeb7f184b7965709825a79 ");
-  }
-  if (message.content.startsWith("!ineedfriends")) {
-    message.channel.send(" ```Thats gone be $50 for 1 fake friend!```");
-  }
-  if (message.content.startsWith("!hkq")) {
-    message.channel.send(" ```Darling , you are a goddess and once you know what that truly means, i pray for anyone who tries to hurt you ```");
+    message.reply(" ```The developer is Rean#4674``` ");
   }
   if (message.content.startsWith("!rean")) {
-    message.channel.send(" ```The Developer of the bod and our holy king ```");
-  }
-  if (message.content.startsWith("!omicron")) {
-    message.channel.send(" ```A Huge Fucking Giant!```");
-  }            
-  if (message.content.startsWith("!penis")) {
-    message.channel.send(" ```8===)```");
-  }                    
-  if (message.content.startsWith("!stefaan")) {
-    message.channel.send(" ```Our speedy runner```");
-  }    
-  if (message.content.startsWith("!myown")) {
-    message.channel.send(" ```Contact Rean#4356 to buy a custom bot!```");
+    message.channel.send(" ```The Developer of the bod ```");
   }   
   if (message.content === '!avatar') {
     message.reply(message.author.avatarURL);
   }
-  if (message.content.startsWith("!charlie")) {
-    message.channel.send(" ```A wise man once said K.I.S Your self and Theeeee and CruMPPPPPits```");
+  if (message.content.startsWith("!ip")) {
+    message.channel.send(" ```Play.InsifeFactions.Com```");
   }
   
 });
 
-client.on('guildMemberAdd', member => {
+client.on('guildMemberAdd', members => {
 
-  const channel = member.guild.channels.find('name', 'member-log');
+  const channel = member.guild.channels.find('name', 'welcome');
 
   if (!channel) return;
-  channel.send(`Welcome to **HELL** ${member}`);
+  channel.send(`Welcome to **InsifeFactions** ${member}`);
 
 });
 
